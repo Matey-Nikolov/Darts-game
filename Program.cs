5601ab82ab5e27b76953014aa9962fd95847b915
Matey-Nikolov <matey.nikolov@mail.bg>
2021-04-13 10:08:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Darts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gameName = Console.ReadLine();
+
+            int points = 301;
+            string SingleDoubleTripleRetire = "";
+            int successFulshots = 0;
+            int unsuccessFulshots = 0;
+
+            while (SingleDoubleTripleRetire != "Retire")
+            {
+                SingleDoubleTripleRetire = Console.ReadLine();
+                if (SingleDoubleTripleRetire == "Retire")
+                {
+                    Console.WriteLine($"{gameName} retired after {unsuccessFulshots} unsuccessful shots.");
+                    break;
+                }
+
+                int point = int.Parse(Console.ReadLine());
+
+                switch (SingleDoubleTripleRetire)
+                {
+                    case "Single":
+                        point = point * 1;
+                        if (point <= points)
+                        {
+                            points = points - point;
+                            successFulshots++;
+                        }
+                        else
+                        {
+                            unsuccessFulshots++;
+                        }
+                        break;
+                    case "Double":
+                        point = point * 2;
+                        if (point <= points)
+                        {
+                            points = points - point;
+                            successFulshots++;
+                        }
+                        else
+                        {
+                            unsuccessFulshots++;
+                        }
+                        break;
+                    case "Triple":
+                        point = point * 3;
+                        if (point <= points)
+                        {
+                            points = points - point;
+                            successFulshots++;
+                        }
+                        else
+                        {
+                            unsuccessFulshots++;
+                        }
+                        break;
+                }
+
+                if (points == 0)
+                {
+                    Console.WriteLine($"{gameName} won the leg with {successFulshots} shots.");
+                    break;
+                }
+            }
+        }
+    }
+}
